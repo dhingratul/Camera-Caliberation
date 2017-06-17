be9e3bebb7ba0d0e9dcb21b37559d579903f2062
dhingratul <atul.dhingra@rutgers.edu>
2017-06-17 13:05:17 -0400

    Computes V based on H, i, j

--- compute_v_ij.m
+++ compute_v_ij.m
@@ -1,4 +1,8 @@
+%% Compute Vertices based on Homographies
+% Input: H-Matrix, i, j
+% Output: V(i,j)
+% Note: Run from camera2dcaliberation.m
 function [v_ij]=compute_v_ij(H,i,j)
-    v_ij=[H(1,i)*H(1,j) , H(1,i)*H(2,j)+H(2,i)*H(1,j) , H(2,i)*H(2,j) ,...
-        H(3,i)*H(1,j)+H(1,i)*H(3,j) , H(3,i)*H(2,j)+H(2,i)*H(3,j) , H(3,i)*H(3,j)]';
+v_ij=[H(1,i)*H(1,j) , H(1,i)*H(2,j)+H(2,i)*H(1,j) , H(2,i)*H(2,j) ,...
+    H(3,i)*H(1,j)+H(1,i)*H(3,j) , H(3,i)*H(2,j)+H(2,i)*H(3,j) , H(3,i)*H(3,j)]';
 end
