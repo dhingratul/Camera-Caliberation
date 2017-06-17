572d001242fc19685d4dfcc30c1c324d951c941f
dhingratul <atul.dhingra@rutgers.edu>
2017-06-17 13:10:19 -0400

    Compute R,t parameters

--- computeRT.m
+++ computeRT.m
@@ -1,7 +1,12 @@
+%% Comput R and T parametes
+% Input: H-Matrix, K-Matrix
+% Output: R, t
+% Note: Run from camera2dcaliberation
 function[R,t]=computeRT(H,A)
-    h1=H(:,1); h2=H(:,2); h3=H(:,3);
+h1=H(:,1); h2=H(:,2); h3=H(:,3);
 lambda=1/norm(inv(A)*h1);
 r1 = lambda*inv(A)*h1;
+lambda=1/norm(inv(A)*h2);
 r2 = lambda*inv(A)*h2;
 r3 = cross(r1,r2);
 t = lambda*inv(A)*h3;
