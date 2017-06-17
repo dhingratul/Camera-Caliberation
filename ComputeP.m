ec258884e7e6d33d68f56019182cc44373d8528e
dhingratul <atul.dhingra@rutgers.edu>
2017-06-17 12:58:22 -0400

    P-Matrix computation

--- ComputeP.m
+++ ComputeP.m
@@ -1,4 +1,9 @@
+%% Compute P-Matrix
+% Computes the Projection matrix
+% Input: M, m
+% Output: PMatrix
+% Note: Run from Inside camera3dcaliberation.m
 function [PMatrix]=ComputeP(M,m)
-    PMatrix=[M 0 0 0 0 m(1,1)*M ; 
-       0 0 0 0 M m(1,2)*M ];
+PMatrix=[M 0 0 0 0 -m(1,1)*M ;
+    0 0 0 0 M -m(1,2)*M ];
 end
